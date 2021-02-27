c6c985180d099fadc7f673b50a8d8ca06c11e372
Akshay Patil <76992034+akshhpatil@users.noreply.github.com>
2021-02-27 18:19:35 +0530

    Update text-to-speech.py

--- text-to-speech.py
+++ text-to-speech.py
@@ -1,1 +1,13 @@
 
+from gtts import gTTS                                     #import modules from gtts
+import os
+
+Myvariable ="Hello Good evening this is demo program of text-to-speech in python." 
+
+language = 'en'                                            #Select language              
+
+output = gTTS(text= Myvariable,lang=language,slow=False)   
+
+output.save("output.mp3")                                   
+
+os.system("start output.mp3")                             #save your file in directory & play when it save
